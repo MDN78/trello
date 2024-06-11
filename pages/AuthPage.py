5c49585a1c75b4871a46f8ffc9aec2136fce602f
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-06-11 15:11:01 +0300

    add first test

--- pages/AuthPage.py
+++ pages/AuthPage.py
@@ -2,6 +2,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+
 # import allure
 # from configuration.ConfigProvider import ConfigProvider
 # from testdata.DataProvider import DataProvider
@@ -19,9 +21,13 @@ class AuthPage:
     def login_as(self, email: str, password: str):
         self.__driver.find_element(By.CSS_SELECTOR, "#username").send_keys(email)
         self.__driver.find_element(By.CSS_SELECTOR, "#login-submit").click()
-        # wating downloading field "password"
-        WebDriverWait(self.__driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "svg[role='presentation']")))
+        # waiting downloading field "password"
+        WebDriverWait(self.__driver, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "svg[role='presentation']")))
         self.__driver.find_element(By.CSS_SELECTOR, "#password").send_keys(password)
         self.__driver.find_element(By.CSS_SELECTOR, "#login-submit").click()
 
-
+    def get_current_url(self):
+        WebDriverWait(self.__driver, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, '.boards-page-section-header-name')))
+        return self.__driver.current_url
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,9 +1,11 @@
 from pages.AuthPage import AuthPage
 import time
 
-def test_first(browser):
+def test_auth(browser):
     auth_page = AuthPage(browser)
     auth_page.go()
     auth_page.login_as("salesakk@gmail.com", "111111Atlas1")
 
-    time.sleep(4)
+    assert auth_page.get_current_url().endswith("/boards")
+
+    time.sleep(2)
