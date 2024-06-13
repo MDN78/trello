71974f2ab1559bc4fdaf613b37d85852652664d6
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-06-13 12:26:03 +0300

    update dates

--- tests/api/test_api_board.py
+++ tests/api/test_api_board.py
@@ -4,24 +4,26 @@ import allure
 
 
 @pytest.mark.skip
-def test_get_boards(api_client: BoardApi):
-    board_list = api_client.get_all_boards_by_org_id('64158b830d737136d0131035')
+def test_get_boards(api_client: BoardApi, testdata: dict):
+    org_id = testdata.get("org_id")
+    board_list = api_client.get_all_boards_by_org_id(org_id)
     assert len(board_list) == 0
     print(board_list)
 
 # @pytest.mark.skip
-def test_create_board(api_client: BoardApi, delete_board: dict):
-    board_list_before = api_client.get_all_boards_by_org_id('64158b830d737136d0131035')
+def test_create_board(api_client: BoardApi, delete_board: dict, testdata: dict):
+    org_id = testdata.get("org_id")
+    board_list_before = api_client.get_all_boards_by_org_id(org_id)
     resp = api_client.create_board('New board to be deleted')
     delete_board["board_id"] = resp.get("id")
-    print(resp)
-    board_list_after = api_client.get_all_boards_by_org_id('64158b830d737136d0131035')
+    board_list_after = api_client.get_all_boards_by_org_id(org_id)
     assert len(board_list_after) - len(board_list_before) == 1
 
 @pytest.mark.skip
-def test_delete_board(api_client: BoardApi, dummy_board_id: str):
-    board_list_before = api_client.get_all_boards_by_org_id('64158b830d737136d0131035')
+def test_delete_board(api_client: BoardApi, dummy_board_id: str, testdata: dict):
+    org_id = testdata.get("org_id")
+    board_list_before = api_client.get_all_boards_by_org_id(org_id)
     resp = api_client.delete_board_by_id(dummy_board_id)
     print(resp)
-    board_list_after = api_client.get_all_boards_by_org_id('64158b830d737136d0131035')
+    board_list_after = api_client.get_all_boards_by_org_id(org_id)
     assert len(board_list_before) - len(board_list_after) == 1
